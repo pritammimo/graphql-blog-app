fb9e43b945d1291d183904b7af8f2e4e2c0b71d7
pritamcn <pritams@capitalnumbers.com>
2022-07-15 10:36:25 +0530

    create with prisma

--- src/Schema.ts
+++ src/Schema.ts
@@ -3,5 +3,36 @@ export const typeDefs=gql`
 type Query{
     hello:String!
 }
-
+type Mutation {
+    postCreate(title:String!,content:String!):PostPayload!
+}
+type Post {
+    id:ID!
+    title:String!
+    content:String!
+    createdAt:String!
+    published:Boolean!
+    user:User!
+}
+type User{
+    id:ID!
+    email:String!
+     name:String!
+    createdAt:String!
+    posts:[Post!]!
+  profile:Profile!
+}
+type Profile{
+    id:ID!
+    bio:String
+    createdAt:String!
+    user:User!
+}
+type UserError{
+    message:String!
+}
+type PostPayload{
+    userErrors:[UserError!]!
+    post:Post
+}
 `
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 import { ApolloServer,gql } from "apollo-server";
 import { typeDefs } from "./Schema";
-import { Query} from "./resolvers";
+import { Query,Mutation} from "./resolvers";
+import { PrismaClient,Prisma } from "@prisma/client";
+
+const prisma=new PrismaClient();
+export interface Context {
+    prisma:PrismaClient<
+    Prisma.PrismaClientOptions,
+    never,
+    Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined
+  >;
+}
 const server=new ApolloServer({
     typeDefs,
     resolvers:{
-        Query
+        Query,
+        Mutation
+    },
+    context:{
+       prisma 
     }
 })
 server.listen().then(({url})=>{
